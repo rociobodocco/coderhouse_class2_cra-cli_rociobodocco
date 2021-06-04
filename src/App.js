2c052181fee70c47e729ee81c035e5be2e4a452f
Rocio Bodocco <rocio.bodocco@gmail.com>
2021-06-03 23:25:06 -0300

    first commit cra cli

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>Hello World with React.JS</h2>
+        <p>by Rocío Bodocco</p>
         <a
           className="App-link"
           href="https://reactjs.org"
